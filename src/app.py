0e4dfd9661bf3f69bb2db469ff038aa6900e1080
Tunji17 <tunjiabioye17@outlook.com>
2019-11-23 16:04:55 +0100

    heroku build

--- src/app.py
+++ src/app.py
@@ -5,8 +5,8 @@ from flask_cors import CORS
 from flask_migrate import Migrate, MigrateCommand
 from pprint import pprint
 from flask_moment import Moment
-from database.models import setup_db, Movie, Actor
-from auth.auth import AuthError, requires_auth
+from .database.models import setup_db, Movie, Actor
+from .auth.auth import AuthError, requires_auth
 
 def create_app(test_config=None):
   # create and configure the app
