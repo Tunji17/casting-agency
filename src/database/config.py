f7da6b30af3d9309f597cc6d015beebc4b6bdc0a
Tunji17 <tunjiabioye17@outlook.com>
2019-11-23 14:37:39 +0100

    heroku build

--- src/database/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-SECRET_KEY = os.urandom(32)
-basedir = os.path.abspath(os.path.dirname(__file__))
-DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'postgres://postgres:0yetunji@localhost:5432/castingagency'
